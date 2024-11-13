78c84605026c2eb8c3695294dd7410abc8b0665f
lucimara1901 <00001098050630sp@al.educacao.sp.gov.br>
2024-11-13 08:41:45 -0300

    Projeto FlashCards

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+   'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual capital dos Estados Unidos?',
+    'A capital dos Estados Unidos é Washington'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'como se diz eu te amo em inglês?',
+    ' eu te amo em inglês é I Love You'
+)
